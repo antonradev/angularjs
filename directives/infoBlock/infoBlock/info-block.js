6b191318703f24a0b264571c2194fc4cf25ca79a
Anton Radev <anton.radev@gmail.com>
2015-01-21 21:30:14 +0200

    Extended functionality. LocalStorage actions added. Saving marked blocks, reading and deleting data from local storage

--- directives/infoBlock/infoBlock/info-block.js
+++ directives/infoBlock/infoBlock/info-block.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ngdirectives')
-        .directive('infoBlock', function () {
+        .directive('infoBlock', function ($localStorage) {
 
             var _linkFn = function link(scope, element, attrs) {
 
@@ -9,13 +9,32 @@ angular.module('ngdirectives')
                     element.find('h3').find('a').addClass('no-ui-sref');
                 }
 
+                element.find('.mark-block').click(function () {
+                    if ($localStorage.markedItems.indexOf(scope.blockid) === -1) {
+                        $localStorage.markedItems.push(parseInt(scope.blockid));
+                        element.addClass('block-is-marked');
+                        scope.$apply();
+                    }
+                    this.remove();
+                });
+
+                // It's outside of the directive scope and the function is not called from the directive
+                scope.$parent.clearItem = function (id) {
+
+                    var i = $localStorage.markedItems.indexOf(id);
+                    if (i !== -1) {
+                        $localStorage.markedItems.splice(i, 1);
+                    }
+                };
+
 
             };
 
             return {
                 restrict: 'EA',
-                templateUrl: 'scripts/directives/infoBlock/info-block.html',
+                templateUrl: '/scripts/directives/infoBlock/info-block.html',
                 scope: {
+                    blockid: '@',
                     blockTitle: '@',
                     blockContent: '@',
                     blockIcon: '@',
--- directives/infoBlock/infoBlockCtrl.js
+++ directives/infoBlock/infoBlockCtrl.js
@@ -6,21 +6,45 @@ angular.module('ngdirectives')
                 url: '/'
             }));
         })
-        .controller('IndexCtrl', function ($scope) {
+        .controller('IndexCtrl', function ($scope, $localStorage) {
 
             $scope.infoBlockData = [
                 {
+                    itemId: 1,
                     itemTitle: 'Title from controller scope',
                     itemContent: 'I am content displayed from the controller scope data',
                     itemUrl: '/lorem-ipsum',
                     itemIcon: 'fa-android'
                 },
                 {
+                    itemId: 2,
                     itemTitle: 'I am from the Controller Data as well',
                     itemContent: 'Just a paragraph with Lorem ipsum content',
                     itemUrl: '/lorem2',
                     itemIcon: 'fa-user'
+                },
+                {
+                    itemId: 3,
+                    itemTitle: 'Hi from the Controller data',
+                    itemContent: 'This is just another block with some data inside of it',
+                    itemUrl: '/hi-controller',
+                    itemIcon: 'fa-paper-plane'
+                },
+                {
+                    itemId: 4,
+                    itemTitle: 'And the winner is the Fourth box',
+                    itemContent: 'Let me put just one more lorem ipsum paragraph here',
+                    itemUrl: '/lorem3',
+                    itemIcon: 'fa-line-chart'
                 }
             ];
+
+            // Default Local Storage values
+            $scope.$storage = $localStorage.$default({
+                markedItems: []
+            });
+
+            $scope.markedItems = $localStorage.markedItems;
+
 
         });
